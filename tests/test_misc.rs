f0b0e403ee91d299909e7dd4a7bb2c591df22b3e
Joshix <joshix.dev@mailbox.org>
2025-05-23 12:00:00 +0000

    add tests

--- tests/test_misc.rs
+++ tests/test_misc.rs
@@ -101,3 +101,29 @@ fn test_new_encoder() {
     let (_header, res) = decode_to_vec(qoi).unwrap();
     assert_eq!(res, [3, 2, 1, 7, 6, 5, 11, 10, 9, 15, 14, 13]);
 }
+
+#[test]
+fn test_changing_out_channels_3to4() {
+    let arr3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]; // 2 * 2 * 3
+
+    let enc = qoi::Encoder::new(&arr3, 2, 2).unwrap();
+    assert_eq!(enc.channels(), Channels::Rgb);
+    let enc = enc.with_channels(Channels::Rgba);
+    assert_eq!(enc.channels(), Channels::Rgba);
+    let qoi = enc.encode_to_vec().unwrap();
+    let (_header, res) = decode_to_vec(qoi).unwrap();
+    assert_eq!(res, [0, 1, 2, 255, 3, 4, 5, 255, 6, 7, 8, 255, 9, 10, 11, 255]);
+}
+
+#[test]
+fn test_changing_out_channels_4to3() {
+    let arr4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]; // 2 * 2 * 4
+
+    let enc = qoi::Encoder::new(&arr4, 2, 2).unwrap();
+    assert_eq!(enc.channels(), Channels::Rgba);
+    let enc = enc.with_channels(Channels::Rgb);
+    assert_eq!(enc.channels(), Channels::Rgb);
+    let qoi = enc.encode_to_vec().unwrap();
+    let (_header, res) = decode_to_vec(qoi).unwrap();
+    assert_eq!(res, [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14]);
+}
